98f18f0fb1d6db22c720eabe9cc9b976ce458f5d
Ali Haugh <haughar@hotmail.com>
2015-11-03 18:26:56 -0800

    Initial Commit

---
+++ iQuiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iQuiz
+//
+//  Created by Alison Rose Haugh on 11/3/15.
+//  Copyright © 2015 Alison Haugh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
